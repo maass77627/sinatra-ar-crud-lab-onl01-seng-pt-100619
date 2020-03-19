5c41abd06cd20500cbc6dcb052c5b9bfad6020f3
kelly r maass <kmaass77627@gmail.com>
2020-03-19 18:12:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,11 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/articles/:id' do
-
+    @article = Article.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save 
+    erb :show
   end
 
 delete  '/articles/:id/delete' do
