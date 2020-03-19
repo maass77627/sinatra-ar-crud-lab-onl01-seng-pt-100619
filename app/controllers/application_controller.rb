58047fb5d301e085755bdecbc4e092d68399b3b3
kelly r maass <kmaass77627@gmail.com>
2020-03-19 18:26:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,6 @@ class ApplicationController < Sinatra::Base
   get '/articles/:id/edit' do
     @article = Article.find_by_id(params[:id])
     erb :edit
-    redirect to '/articles/:id'
   end
 
   patch '/articles/:id' do
