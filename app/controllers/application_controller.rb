fdd233aaceec79c3ab0990105ad46ef10c55b46b
kelly r maass <kmaass77627@gmail.com>
2020-03-19 17:04:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 
   post '/articles' do
       @article = Article.create(title: params[:title], content: params[:content])
-      redirect to "/articles/#{@user.id}"
+      redirect to "/articles/#{@article.id}"
   end
 
   get '/articles/:id' do
